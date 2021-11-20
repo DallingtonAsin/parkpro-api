82a779b4cb846938b6939606c56195fa42d27ed5
Dallington <asingwire50dallington@gmail.com>
2021-11-20 08:36:37 +0300

    changes dated 20/11/2021 08:36

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -511,10 +511,10 @@ class CustomerController extends Controller
     public function uploadProfilePicture(Request $request){
         $resp = new ApiResponse();
         try {
-            return response()->json($request->all());
+            // return response()->json($request->all());
             $authToken   =   $request->header('AuthToken');
             if (!empty($authToken) && TokenAuth::validate($authToken)) {
-                if($request->filled('user_id') && $request->filled('phone_number') && $request->input('image')){
+                if($request->filled('user_id') && $request->filled('phone_number') && $request->filled('image')){
                     
                     $file = $request->file('image');
                     $customer_id = $request->input('user_id');
