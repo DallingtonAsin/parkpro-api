e3739562a99d492831a7e6ec0ebb0dca33e31b3c
Dallington <asingwire50dallington@gmail.com>
2021-12-18 16:53:26 +0300

    changes dated 18/12/2021 16:53

--- app/Http/Controllers/VehicleCategoryController.php
+++ app/Http/Controllers/VehicleCategoryController.php
@@ -20,9 +20,13 @@ class VehicleCategoryController extends Controller
         $resp = new ApiResponse();
         try {
             $vehicle_categories = VehicleCategory::all();
+            foreach($vehicle_categories as $cat){
+                $cat->name = strtolower($cat->name);
+            }
             $resp->statusCode = Globals::$STATUS_CODE_SUCCESS;
             $resp->message  = Globals::$STATUS_DESC_SUCCESS;
             $resp->data = $vehicle_categories;
+
         } catch (\Exception $ex) {
             $resp->statusCode = Globals::$STATUS_CODE_ERROR;
             $resp->message = Globals::$STATUS_DESC_ERROR;
