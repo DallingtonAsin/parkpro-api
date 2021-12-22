9d8a6a1a21366d12322cfc06fc22640083afefa4
Dallington <asingwire50dallington@gmail.com>
2021-12-22 16:45:35 +0300

    changes dated 22/12/2021 16:45

--- app/Http/Controllers/ParkingRequestController.php
+++ app/Http/Controllers/ParkingRequestController.php
@@ -372,7 +372,7 @@ class ParkingRequestController extends Controller
                                 $info->booking_period = $info->start_time."-".$info->end_time;
                                 $info->parking_area = ParkingArea::where('id', $info->parking_area_id)->value('name');
                                 $info->car_type = VehicleCategory::where('id', $info->vehicle_category_id)->value('name');
-                                $info->fee_per_hour = VehicleCategory::where('id', $info->vehicle_category_id)->value('fee_per_hour');
+                                $info->fee_per_hour = ParkingFee::where('vehicle_cat_id', $info->vehicle_cat_id)->value('fee_per_hour');
                                 $info->approval_date = date('Y-m-d H:i A', strtotime($request->approval_date));
                                 $ino->amount = number_format($request->amount);
                             }
