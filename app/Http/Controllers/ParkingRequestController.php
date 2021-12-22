329bdfddd41d28821703b6ef4f73e4f18ee2d7f6
Dallington <asingwire50dallington@gmail.com>
2021-12-22 16:15:01 +0300

    changes dated 22/12/2021 16:14

--- app/Http/Controllers/ParkingRequestController.php
+++ app/Http/Controllers/ParkingRequestController.php
@@ -367,6 +367,14 @@ class ParkingRequestController extends Controller
                     if($request->filled('id')){
                         $id = $request->input('id');
                         $requests = ParkingRequest::where('customer_id', $id)->get();
+                        if(count((array)$requests) > 0){
+                            foreach($requests as $request){
+                                $request->approval_date = date('Y-m-d H:i A', strtotime($request->approval_date));
+                                $request->amount = number_format($request->amount);
+                            }
+                        }else{
+                            $requests = [];
+                        }
                         $this->response['statusCode'] = Globals::$STATUS_CODE_SUCCESS;
                         $this->response['message'] = "Parking requests data found";
                         $this->response['data'] = $requests;
