a9ea8012721b6462f3c7b4b409b82432f9c1be31
Dallington <asingwire50dallington@gmail.com>
2021-12-23 09:02:28 +0300

    changes dated 23/12/2021 09:02

--- app/Http/Controllers/ParkingRequestController.php
+++ app/Http/Controllers/ParkingRequestController.php
@@ -372,7 +372,7 @@ class ParkingRequestController extends Controller
                             foreach($order as $info){
                                 $start_time = date('H:i', strtotime($info->start_time));
                                 $end_time = date('H:i', strtotime($info->end_time));
-                                $info->booking_period = $start_time."-".$end_time;
+                                $info->booking_period = $start_time." - ".$end_time;
                                 $customer = Customer::find($info->customer_id);
                                 $info->name = $customer->first_name." ".$customer->last_name;
                                 $info->telephone_no = $customer->phone_number;
