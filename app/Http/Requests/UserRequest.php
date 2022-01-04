6d1621a1f0fd69b2bb594bc46ef922f3f366c03d
DallingtonAsin <asingwire50dallington@gmail.com>
2022-01-04 16:43:33 +0300

    changes dated 04/01/2022 16:43

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUserRequest extends FormRequest{
+
+    // Determine if the user is authorized to make this request
+
+    public function authorize(){
+        return true;
+    }
+
+    // Get the validation rules that apply to the request
+    // @return array
+
+    public function rules()
+{
+    return [
+        'invoice_date' => 'required|date',
+        'consumer_name' => 'required',
+        'products' => 'required|array',
+        'quantities' => 'required|array',
+        'prices' => 'required|array',
+
+    ];
+}
+
+}
--- app/Services/CustomerService.php
+++ app/Services/CustomerService.php
@@ -1,8 +1,27 @@
 <?php
+
+
 namespace App\Services\CustomerService;
+use App\Models\Customer;
 
 class CustomerService{
 
+  public function storeNewCustomer(
+      string $invoice_date,
+      string $customer_name,
+      array $products,
+      array $quantities,
+      array $prices
+  ): Customer{
 
-    
+    $invoice = Invoice::create([
+        'invoice_number' => $this->getNextInvoiceNumber(),
+        'invoice_date' => $invoice_date,
+        'customer_name' => $customer_name,
+    ]);
+
+    return $invoice;
+
+  }
+
 }
